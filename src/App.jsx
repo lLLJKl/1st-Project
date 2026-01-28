604bcc62ab52bd8f3b375cfa1d697cab1cab84d4
delight <mpsddd@gmail.com>
2026-01-29 03:51:28 +0900

    정리해야할것들

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "@/pages/Homepage";
+import MainLayout from "@/layouts/MainLayout";
+import Home from "@/pages/Home";
+import EnergyData from "@/pages/EnergyData";
+import EmissionData from "@/pages/EmissionData";
+import CostData from "@/pages/CostData";
+import Signin from "@/pages/Signin";
+import FindPassword from "@/pages/FindPassword";
+import MyPage from "@/pages/Mypage";
+// import Signup from "@/pages/Signup";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/homepage" element={<HomePage />} />
+        {/* <Route path="/signup" element={<Signup />} /> */}
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/Signin" element={<Signin />} />
+          <Route path="/FindPassword" element={<FindPassword />} />
+          <Route path="/mypage" element={<MyPage />} />
+        {/* <Route path="/projects" element={<Projects />} /> */}
+        {/* <Route path="/projects/new" element={<ProjectCreate />} /> */}
+
+        {/* 데이터셋 */}
+        <Route path="/data/energy" element={<EnergyData />} />
+        <Route path="/data/emission" element={<EmissionData />} />
+        <Route path="/data/cost" element={<CostData />} />
+        </Route> 
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+import "@/styles/home.css";
+
+const HomePage = () => {
+  return (
+    <div className="home-page d-flex justify-content-center align-items-center">
+      <div className="text-center text-white px-3">
+        <h1 className="fw-bold mb-3">
+          Welcome to Our Service
+        </h1>
+
+        <p className="mb-4 text-white-50">
+          로그인 후 서비스를 이용하실 수 있습니다
+        </p>
+
+        <div className="d-flex justify-content-center gap-3">
+          <Link to="/signin" className="btn btn-light px-4">
+            로그인
+          </Link>
+
+          <Link to="/signup" className="btn btn-outline-light px-4">
+            회원가입
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
